ad3a1ec60ffaa31863378c591f0f1c58772d6ace
Boris Gusev <bgusev@gmail.com>
2022-10-06 12:44:54 +0200

    main functionality of molecule class ported over many methods were ommitted (e.g. accessing attributes) the missing methods can be added if later found necessary

--- src/glycosylator/molecule/molecule_changes.py
+++ src/glycosylator/molecule/molecule_changes.py
@@ -1,6 +1,5 @@
-from collections import deque
-
 import networkx as nx
+import numpy as np
 import prody
 
 
@@ -104,6 +103,80 @@ class Molecule:
 
         self.torsionals = torsionals
 
+    def measure_dihedral_angle(self, dihedral_atoms):
+        """Calculates dihedral angle for 4 atoms.
+        Parameters:
+            torsional: list of atom serial numbers
+        Returns:
+            angle: dihedral angle in degrees
+        """
+
+        idx = np.argsort(dihedral_atoms)
+        vec_sel = self.atom_group.select(f"index {' '.join(map(str, dihedral_atoms))}")
+        c0, c1, c2, c3 = vec_sel.getCoords()[idx, :]
+
+        q1 = c1 - c0
+        q2 = c2 - c1
+        q3 = c3 - c2
+
+        q1xq2 = np.cross(q1, q2)
+        q2xq3 = np.cross(q2, q3)
+
+        n1 = q1xq2 / np.sqrt(np.dot(q1xq2, q1xq2))
+        n2 = q2xq3 / np.sqrt(np.dot(q2xq3, q2xq3))
+
+        u1 = n2
+        u3 = q2 / (np.sqrt(np.dot(q2, q2)))
+        u2 = np.cross(u3, u1)
+
+        cos_theta = np.dot(n1, u1)
+        sin_theta = np.dot(n1, u2)
+        angle = np.degrees(-np.arctan2(sin_theta, cos_theta))
+
+        return angle
+
+    def get_all_torsional_angles(self):
+        """Computes all the torsional angles of the molecule
+        Return:
+            angles: angles in degrees
+        """
+        return [self.measure_dihedral_angle(torsional) for torsional in self.torsionals]
+
+    def rotate_bond(self, torsional, theta, absolute=False):
+        """Rotate the molecule around a torsional angle. Atom affected are in direct graph.
+        Parameters:
+            torsional: index of torsional angle (in torsionals) or list of indices of atoms defining the torsional angle.
+            theta: amount (degrees)
+            absolute: defines if theta is the increment or the absolute value of the angle
+        Returns:
+            c_angle: angle before the rotation in degrees
+        """
+        if type(torsional) == int:
+            torsional = self.torsionals[torsional]
+        elif torsional not in self.torsionals:
+            raise ValueError("Invalid Torsional")
+
+        atoms = []
+        a1 = torsional[-2]
+
+        atoms = nx.descendants(self.bond_graph, a1)
+        sel = self.atom_group.select(f"index {' '.join(map(str, atoms))}")
+        t = torsional[1:-1]
+        v1, v2 = self.atom_group.select(f"index {' '.join(map(str, t))}").getCoords()[
+            np.argsort(t), :
+        ]
+        axis = v2 - v1
+        c_angle = 0.0
+        if absolute:
+            c_angle = self.measure_dihedral_angle(torsional)
+            theta = theta - c_angle
+
+        coords = sel.getCoords()
+        M = _rotation_matrix(axis, np.radians(theta))
+        coords = M.dot(coords.transpose())
+        sel.setCoords(coords.transpose() + v2 - np.dot(M, v2))
+        return c_angle
+
 
 def _validate_atom_group(atom_group: prody.AtomGroup):
     """validates that the AtomGroup consists of a single physical molecule"""
@@ -136,3 +209,30 @@ def _find_paths(G, node, length, excludeSet=None):
     ]
     excludeSet.remove(node)
     return paths
+
+
+def _rotation_matrix(axis, theta):
+    """Computes the rotation matrix about an arbitrary axis in 3D
+    Code from: http://stackoverflow.com/questions/6802577/python-rotation-of-3d-vector
+    Parameters:
+        axis: axis
+        theta: rotation angle
+    Return:
+        rotation matrix
+
+    """
+
+    axis = np.asarray(axis)
+    theta = np.asarray(theta)
+    axis = axis / math.sqrt(np.dot(axis, axis))
+    a = math.cos(theta / 2.0)
+    b, c, d = -axis * math.sin(theta / 2.0)
+    aa, bb, cc, dd = a * a, b * b, c * c, d * d
+    bc, ad, ac, ab, bd, cd = b * c, a * d, a * c, a * b, b * d, c * d
+    return np.array(
+        [
+            [aa + bb - cc - dd, 2 * (bc + ad), 2 * (bd - ac)],
+            [2 * (bc - ad), aa + cc - bb - dd, 2 * (cd + ab)],
+            [2 * (bd + ac), 2 * (cd - ab), aa + dd - bb - cc],
+        ]
+    )
