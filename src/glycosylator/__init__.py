6653f0d936d5155217d7f9be18c00b08f6d11410
Boris Gusev <bgusev@gmail.com>
2022-10-06 15:19:13 +0200

    direct class import and import *

---
+++ src/glycosylator/__init__.py
@@ -0,0 +1,17 @@
+from .charmm_parameters import CHARMMParameters
+from .charmm_topology import CHARMMTopology
+from .drawer import Drawer
+from .glycosylator import Glycosylator
+from .molecule import Molecule
+from .molecule_builder import MoleculeBuilder
+from .sampler import Sampler
+
+__all__ = [
+    CHARMMParameters,
+    CHARMMTopology,
+    Drawer,
+    Glycosylator,
+    Molecule,
+    MoleculeBuilder,
+    Sampler,
+]
