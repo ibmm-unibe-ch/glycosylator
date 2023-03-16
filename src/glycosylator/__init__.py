bef439fb57e59399e9af2fe7a35e487618541985
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-03-16 17:36:29 +0100

    include new force_fields module in init +++ update glycan_representation references to graphs

--- src/glycosylator/__init__.py
+++ src/glycosylator/__init__.py
@@ -1,16 +1,22 @@
-from glycosylator import file_parsers, glycan_representations
-from glycosylator.drawer import Drawer
-from glycosylator.glycosylator import Glycosylator
-from glycosylator.molecule import Molecule
-from glycosylator.molecule_builder import MoleculeBuilder
-from glycosylator.sampler import Sampler
+# from glycosylator.drawer import Drawer
+# from glycosylator.Glycosylator import Glycosylator
+# from glycosylator.molecule import Molecule
+# from glycosylator.molecule_builder import MoleculeBuilder
+# from glycosylator.sampler import Sampler
+import glycosylator.utils as utils
+import glycosylator.resources as resources
+import glycosylator.graphs as graphs
+import glycosylator.force_fields as force_fields
 
 __all__ = [
-    file_parsers,
-    glycan_representations,
-    Drawer,
-    Glycosylator,
-    Molecule,
-    MoleculeBuilder,
-    Sampler,
+    force_fields,
+    # graphs,
+    # Drawer,
+    # Glycosylator,
+    # Molecule,
+    # MoleculeBuilder,
+    # Sampler,
+    utils,
+    resources,
+    graphs,
 ]
