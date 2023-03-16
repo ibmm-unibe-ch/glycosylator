b2b82bfcfbceced18c50be4cd419de74ec0e793a
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-03-16 17:31:40 +0100

    refactor the AtomGraph/ResidueGraph into their own module

---
+++ src/glycosylator/graphs/ResidueGraph.py
@@ -0,0 +1,93 @@
+import networkx as nx
+import numpy as np
+import mdtraj as md
+import Bio.PDB as bio
+
+import glycosylator.utils as utils
+from glycosylator.graphs.BaseGraph import BaseGraph
+import glycosylator.graphs.AtomGraph as AtomGraph
+
+
+class ResidueGraph(BaseGraph):
+    """
+    A graph representation of residues bonded together as an abstraction of a large contiguous molecule.
+    """
+
+    @classmethod
+    def from_AtomGraph(cls, atom_graph):
+        """
+        Create a ResidueGraph from an AtomGraph.
+
+        Parameters
+        ----------
+        atom_graph : AtomGraph
+            The AtomGraph representation of the molecule
+
+        Returns
+        -------
+        ResidueGraph
+            The ResidueGraph representation of the molecule
+        """
+        id = atom_graph.id
+        # we first get all the atom-resolution bonds between different residues,
+        # then we get the participating parents (e.g. residues) and make the edges from them.
+        bonds = utils.infer_residue_connections(atom_graph.structure)
+        bonds = [(p1.get_parent(), p2.get_parent()) for p1, p2 in bonds]
+        return cls(id, bonds)
+
+    @classmethod
+    def from_pdb(
+        cls,
+        filename: str,
+        id=None,
+        apply_standard_bonds: bool = True,
+        infer_bonds: bool = False,
+        max_bond_length: float = None,
+        restrict_residues: bool = True,
+    ):
+        """
+        Create a ResidueGraph from a PDB of a single molecule
+
+        Parameters
+        ----------
+        filename : str
+            Path to the PDB file
+        id : str
+            The ID of the molecule. By default the filename is used.
+        apply_standard_bonds : bool
+            Whether to apply standard bonds from known molecule connectivities.
+        infer_bonds : bool
+            Whether to infer bonds from the distance between atoms.
+        max_bond_length : float
+            The maximum distance between atoms to infer a bond.
+        restrict_residues : bool
+            Whether to restrict to atoms of the same residue when inferring bonds.
+
+        Returns
+        -------
+        ResidueGraph
+            The ResidueGraph representation of the molecule
+        """
+        _atom_graph = AtomGraph.from_pdb(
+            filename,
+            id,
+            apply_standard_bonds,
+            infer_bonds,
+            max_bond_length,
+            restrict_residues,
+        )
+
+        return cls.from_AtomGraph(_atom_graph)
+
+
+if __name__ == '__main__':
+
+    _man = "support/examples/MAN9.pdb"
+    man = ResidueGraph.from_pdb(_man)
+
+    import matplotlib.pyplot as plt
+    import networkx as nx
+
+    print(len(list(man.bonds)))
+    nx.draw(man)
+    plt.show()
---
+++ src/glycosylator/graphs/__init__.py
@@ -0,0 +1,9 @@
+"""
+This module defines classes to represent PDB structures as graph objects.
+At the base is the `AtomGraph` object which directly translates into a PDB structure and can be saved as one. 
+It stores all atoms and their connectivity. As an abstraction, the `AtomGraph` can produce a `ResidueGraph` object which represents each residue as a sphere of a certain volume
+instead of all individually contained atoms.
+"""
+
+from glycosylator.graphs.AtomGraph import AtomGraph
+from glycosylator.graphs.ResidueGraph import ResidueGraph
