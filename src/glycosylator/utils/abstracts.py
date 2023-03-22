d529c5ff8c90911913fa9e52b7e7b707b4780314
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-03-22 14:21:26 +0100

    update abstracts

--- src/glycosylator/utils/abstracts.py
+++ src/glycosylator/utils/abstracts.py
@@ -7,6 +7,8 @@ import attr
 import Bio.PDB as bio
 import numpy as np
 
+import glycosylator.utils.structural as structural
+
 
 class AbstractEntity:
     """
@@ -23,7 +25,7 @@ class AbstractEntity:
     def atoms(self):
         return list(self._atoms.values())
 
-    def get_atom(self, id):
+    def get_atom(self, id) -> "AbstractAtom":
         """
         Get an atom by its ID
 
@@ -78,7 +80,26 @@ class AbstractEntity:
             _atom = _atom.id
         return _atom in self._atoms
 
-    def get_bond(self, id1, id2):
+    def get_missing_atoms(self, obj):
+        """
+        Get a list of atoms that are missing from the entity
+
+        Parameters
+        ----------
+        obj
+            The object to compare to which hosts atoms and has a `get_atoms()` method.
+            E.g. a `bio.PDB.Residue`.
+
+        Returns
+        -------
+        missing_atoms: list
+            A list of atoms that are missing from the entity
+        """
+        _atoms = set(i.id for i in obj.get_atoms())
+        missing_atoms = [atom for atom in self.atoms if atom.id not in _atoms]
+        return missing_atoms
+
+    def get_bond(self, id1, id2) -> "AbstractBond":
         """
         Get a bond by its atom IDs
         """
@@ -87,30 +108,86 @@ class AbstractEntity:
         if isinstance(id2, AbstractAtom):
             id2 = id2.id
         for bond in self.bonds:
-            if bond[0].id == id1 and bond[1].id == id2:
+            if bond.atom1.id == id1 and bond.atom2.id == id2:
                 return bond
-            elif bond[0].id == id2 and bond[1].id == id1:
+            elif bond.atom1.id == id2 and bond.atom2.id == id1:
                 return bond
         return None
 
-    def get_internal_coordinates(self, id1, id2, id3, id4):
+    def get_internal_coordinates(self, *ids, mode: str = "exact"):
         """
-        Get an internal coordinate by its atom IDs
+        Get internal coordinates by their atom IDs
+
+        Parameters
+        ----------
+        ids: str
+            The IDs of the atoms in the internal coordinate (this can also be any data object that has an `id` attribute)
+        mode: str
+            The mode to use for matching the internal coordinates.
+            Supported modes are:
+            - `exact`: The internal coordinate must match the given atom IDs exactly (requires that four ids are given)
+            - `partial`: The internal coordinate must match the given atom IDs where they are provided, but wildcards can be set to None (requires that four ids or None are given in order)
+            - `multi_partial`: The internal coordinate must match any given atom IDs where they are provided, but wildcards can be set to None (requires that four ids or None are given in order)
+            - `anywhere`: The internal coordinate must contain all of the given atom IDs in any order (requires at least one id)
+            - `anywhere_partial`: The internal coordinate must contain any of the given atom IDs in any order (requires at least one id)
+
+        Returns
+        -------
+        ics: list
+            A list of internal coordinates
         """
-        if isinstance(id1, AbstractAtom):
-            id1 = id1.id
-        if isinstance(id2, AbstractAtom):
-            id2 = id2.id
-        if isinstance(id3, AbstractAtom):
-            id3 = id3.id
-        if isinstance(id4, AbstractAtom):
-            id4 = id4.id
-        for ic in self.internal_coordinates:
-            if ic[0].id == id1 and ic[1].id == id2 and ic[2].id == id3 and ic[3].id == id4:
-                return ic
-            elif ic[0].id == id4 and ic[1].id == id3 and ic[2].id == id2 and ic[3].id == id1:
-                return ic
-        return None
+
+        if len(ids) == 0:
+            return self.internal_coordinates
+
+        ids = tuple(i.id if hasattr(i, "id") else i for i in ids)
+
+        if mode == "exact":
+
+            if len(ids) != 4:
+                raise ValueError("Exact mode requires that four ids are given to match the internal coordinates")
+
+            for ic in self.internal_coordinates:
+                if ids == ic.ids or ids[::-1] == ic.ids:
+                    return [ic]
+            return []
+
+        elif mode == "partial":
+
+            if len(ids) != 4:
+                raise ValueError("Partial mode requires that four ids or None are given to match the internal coordinates")
+
+            ids = np.array(ids)
+            mask = ids != None
+
+            ics = [ic for ic in self.internal_coordinates if np.all(ids[mask] == np.array(ic.ids)[mask])]
+            return ics
+
+        elif mode == "anywhere":
+
+            ids = set(ids)
+            ics = [ic for ic in self.internal_coordinates if ids.issubset(set(ic.ids))]
+            return ics
+
+        elif mode == "anywhere_partial":
+
+            ids = set(ids)
+            ics = [ic for ic in self.internal_coordinates if len(ids.intersection(set(ic.ids))) != 0]
+            return ics
+
+        elif mode == "multi_partial":
+
+            if len(ids) != 4:
+                raise ValueError("Multi partial mode requires that four ids or None are given to match the internal coordinates")
+
+            ids = np.array(ids)
+            mask = ids != None
+
+            ics = [ic for ic in self.internal_coordinates if np.any(ids[mask] == np.array(ic.ids)[mask])]
+            return ics
+
+        else:
+            raise ValueError(f"Unknown mode {mode}")
 
     def add_atom(self, atom):
         """
@@ -151,28 +228,29 @@ class AbstractResidue(AbstractEntity):
         self.CHARMM_id = None
 
 
-@attr.s
+@attr.s(hash=True)
 class AbstractAtom:
     """
     A representation of a single Atom
     """
 
-    id = attr.ib(type=str)
-    type = attr.ib(default=None, type=str)
+    id = attr.ib(type=str, hash=True)
+    type = attr.ib(default=None, type=str, hash=True)
     charge = attr.ib(default=None, type=float, repr=False)
     mass = attr.ib(default=None, type=float, repr=False)
-    element = attr.ib(default=None, type=str, repr=False)
+    _element = attr.ib(default=None, type=str, repr=False)
     _parent = attr.ib(default=None, repr=False)
     is_wildcard = attr.ib(default=False, type=bool, repr=False)
 
-    def to_biopython(self, parent=None):
+    @property
+    def element(self):
+        if self._element is None:
+            return self.id[0]
+        return self._element
+
+    def to_biopython(self):
         """
         Returns a Bio.PDB.Atom object
-
-        Parameters
-        ----------
-        parent: Bio.PDB.Residue
-            The parent residue of the atom
         """
         new = bio.Atom.Atom(
             self.id,
@@ -185,10 +263,6 @@ class AbstractAtom:
             element=self.element,
             pqr_charge=self.charge,
         )
-        if parent:
-            new.set_parent(parent)
-        elif self._parent:
-            new.set_parent(self._parent)
         return new
 
     def get_parent(self):
@@ -261,6 +335,9 @@ class AbstractBond:
         self.atom1 = atom1 if atom1 else self.atom1
         self.atom2 = atom2 if atom2 else self.atom2
 
+    def __get_item__(self, key):
+        return self.atoms[key]
+
     def __repr__(self):
         return f"AbstractBond({self.atom1.id}, {self.atom2.id})"
 
@@ -269,7 +346,37 @@ class AbstractBond:
 class AbstractInternalCoordinates:
     """
     A representation of a single Internal Coordinate
+
+    Parameters
+    ----------
+    atom1
+        The first atom in the internal coordinate
+    atom2
+        The second atom in the internal coordinate
+    atom3
+        The third atom in the internal coordinate
+    atom4
+        The fourth atom in the internal coordinate
+    bond_length_12
+        The bond length between atoms 1 and 2
+    bond_length_34
+        The bond length between atoms 3 and 4
+    bond_angle_123
+        The bond angle between atoms 1, 2 and 3
+    bond_angle_234
+        The bond angle between atoms 2, 3 and 4
+    dihedral
+        The dihedral angle between atoms 1, 2, 3 and 4
+    bond_length_13
+        The bond length between atoms 1 and 3 (optional, for impropers)
+    improper
+        Whether the internal coordinate is an improper (optional)
     """
+
+    # With this set of data, the position of atom 1 may be determined based on the
+    # positions of atoms 2-4, and the position of atom 4 may be determined from the
+    # positions of atoms 1-3, allowing the recursive generation of coordinates for
+    # all atoms in the structure based on a three-atom seed.
 
     atom1 = attr.ib(type=Union[AbstractAtom, str])
     atom2 = attr.ib(type=Union[AbstractAtom, str])
@@ -308,6 +415,13 @@ class AbstractInternalCoordinates:
         return (self.atom1, self.atom2, self.atom3, self.atom4)
 
     @property
+    def ids(self):
+        """
+        Returns the ids of the atoms that constitute the internal coordinate
+        """
+        return (self.atom1.id, self.atom2.id, self.atom3.id, self.atom4.id)
+
+    @property
     def is_improper(self):
         """
         Returns True if the internal coordinate is improper
@@ -321,11 +435,44 @@ class AbstractInternalCoordinates:
         """
         return not self.improper
 
-    def get_parent(self):
+    def get_reference_atoms(self, _src):
         """
-        Get the parent of the Internal Coordinate (i.e. it's residue)
+        Get reference atoms with available coordinates from a source.
+        Note, that this method does not check if reference coordinates
+        are actually available, so make sure to only provide source objects
+        that have cartesian coordinates contained.
+
+        Parameters
+        ----------
+        _src : object
+            The source object to get the reference atom from. This can be:
+            - a list or tuple of Atoms
+            - an object with a `get_atoms` method (e.g. a biopython Residue)
+
+        Returns
+        -------
+        list
+            A list of reference atoms with available coordinates
         """
-        return self.atom1.get_parent()
+
+        if hasattr(_src, "get_atoms"):
+            _src = _src.get_atoms()
+        elif not isinstance(_src, (list, tuple, set)):
+            raise ValueError("Invalid source object")
+
+        _src = {a.id: a for a in _src}
+        _set_src = set(_src.keys())
+
+        _intersection = set(self.ids).intersection(_set_src)
+        if len(_intersection) == 0:
+            return []
+
+        _ref_atoms = []
+        for _id in self.ids:
+            if _id in _intersection:
+                _ref_atoms.append(_src[_id])
+
+        return _ref_atoms
 
     def __repr__(self):
         return f"AbstractInternalCoordinates({self.atom1.id}, {self.atom2.id}, {self.atom3.id}, {self.atom4.id})"
