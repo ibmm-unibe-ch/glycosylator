ae405f124ac399ef9e8b683b44d96b1d18cd001c
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-03-23 19:20:01 +0100

    add 3d molecule viewer
    add 3d molecule viewer

--- src/glycosylator/utils/__init__.py
+++ src/glycosylator/utils/__init__.py
@@ -6,3 +6,4 @@ from glycosylator.utils.constants import *
 from glycosylator.utils.defaults import *
 import glycosylator.utils.structural as structural
 import glycosylator.utils.abstract as abstract
+import glycosylator.utils.visual as visual
