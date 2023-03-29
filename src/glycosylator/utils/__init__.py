02709ff83806efa0122f0534b1a60c5f6b677c36
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-03-29 11:53:46 +0200

    restructure utils

--- src/glycosylator/utils/__init__.py
+++ src/glycosylator/utils/__init__.py
@@ -1,9 +1,12 @@
 """
 Utility and auxiliary functions and constants used by Glycosylator.
 """
-from glycosylator.utils.auxiliary import *
-from glycosylator.utils.constants import *
-from glycosylator.utils.defaults import *
+import glycosylator.utils.constants as constants
+import glycosylator.utils.defaults as defaults
 import glycosylator.utils.structural as structural
 import glycosylator.utils.abstract as abstract
 import glycosylator.utils.visual as visual
+import glycosylator.utils.convert as convert
+
+from glycosylator.utils.auxiliary import *
+from glycosylator.utils.defaults import get_default_topology, get_default_parameters, get_default_instance
