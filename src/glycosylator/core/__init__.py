0bbd375bc4b65b24c84fcecdef1639c18526cb62
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-04-21 23:15:03 +0200

    add Scaffold class to attach Molecules onto

--- src/glycosylator/core/__init__.py
+++ src/glycosylator/core/__init__.py
@@ -3,7 +3,9 @@ The core classes and functions of Glycosylator
 """
 
 from glycosylator.core.molecule import Molecule
+from glycosylator.core.scaffold import Scaffold
 
 __all__ = [
     "Molecule",
+    "Scaffold",
 ]
