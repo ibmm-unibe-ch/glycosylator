787337ab4d2fc4384df4fe2ebce05e033c624030
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-04-21 23:13:06 +0200

    remove NglViewer (for now)

--- src/glycosylator/utils/visual.py
+++ src/glycosylator/utils/visual.py
@@ -6,32 +6,33 @@ import plotly.graph_objects as go
 import plotly.express as px
 import numpy as np
 from copy import deepcopy
-import nglview
+
+# import nglview
 
 
-class NglViewer:
-    """
-    View a molecule or graph object in 3D using
-    the NGLView library.
+# class NglViewer:
+#     """
+#     View a molecule or graph object in 3D using
+#     the NGLView library.
 
-    Parameters
-    ----------
-    molecule
-        The molecule to view. This may be any object that holds
-        a biopython structure e.g. a Molecule, AtomGraph, or ResidueGraph.
-    """
+#     Parameters
+#     ----------
+#     molecule
+#         The molecule to view. This may be any object that holds
+#         a biopython structure e.g. a Molecule, AtomGraph, or ResidueGraph.
+#     """
 
-    def __init__(self, molecule):
-        if molecule.__class__.__name__ in ("Molecule", "AtomGraph", "ResidueGraph"):
-            self.mol = molecule.structure
-        else:
-            self.mol = molecule
+#     def __init__(self, molecule):
+#         if molecule.__class__.__name__ in ("Molecule", "AtomGraph", "ResidueGraph"):
+#             self.mol = molecule.structure
+#         else:
+#             self.mol = molecule
 
-    def show(self):
-        """
-        Show the molecule in a Jupyter notebook
-        """
-        return nglview.show_biopython(self.mol.structure)
+#     def show(self):
+#         """
+#         Show the molecule in a Jupyter notebook
+#         """
+#         return nglview.show_biopython(self.mol.structure)
 
 
 class MoleculeViewer3D:
