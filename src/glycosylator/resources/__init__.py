2f557d4cb7b2afe660ce0407f10467bedc032c46
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-05-02 11:51:56 +0200

    move import charmm down

--- src/glycosylator/resources/__init__.py
+++ src/glycosylator/resources/__init__.py
@@ -1,13 +1,12 @@
 """
 Additional data resources
 """
-import glycosylator.resources.charmm as charmm
 import glycosylator.resources.pdbe_compounds as pdbe_compounds
 import glycosylator.resources.sequons as sequons
+import glycosylator.resources.charmm as charmm
 
 
 PDBECompounds = pdbe_compounds.PDBECompounds
 CHARMMTopology = charmm.CHARMMTopology
 CHARMMParameters = charmm.CHARMMParameters
-
 SEQUONS = sequons.SEQUONS
