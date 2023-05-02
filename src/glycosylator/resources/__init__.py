dc3f443fca703b23940fb92c1d92024f9a30c6b7
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-05-02 10:34:24 +0200

    update init

--- src/glycosylator/resources/__init__.py
+++ src/glycosylator/resources/__init__.py
@@ -3,7 +3,7 @@ Additional data resources
 """
 import glycosylator.resources.charmm as charmm
 import glycosylator.resources.pdbe_compounds as pdbe_compounds
-from glycosylator.resources.sequons import sequons
+import glycosylator.resources.sequons as sequons
 
 
 PDBECompounds = pdbe_compounds.PDBECompounds
