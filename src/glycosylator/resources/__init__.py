f9c5c016e9e951d44b09304497c8bb34adff2fd8
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-05-02 10:31:12 +0200

    update init

--- src/glycosylator/resources/__init__.py
+++ src/glycosylator/resources/__init__.py
@@ -3,8 +3,11 @@ Additional data resources
 """
 import glycosylator.resources.charmm as charmm
 import glycosylator.resources.pdbe_compounds as pdbe_compounds
-from glycosylator.resources.sequons import SEQUONS as SEQUONS
+from glycosylator.resources.sequons import sequons
+
 
 PDBECompounds = pdbe_compounds.PDBECompounds
 CHARMMTopology = charmm.CHARMMTopology
 CHARMMParameters = charmm.CHARMMParameters
+
+SEQUONS = sequons.SEQUONS
