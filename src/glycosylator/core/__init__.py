e1a0c28f6eb4188c60103e080aedc16dd4ab186c
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-05-16 11:48:53 +0200

    fix type "linkeage" -> "linkage"

--- src/glycosylator/core/__init__.py
+++ src/glycosylator/core/__init__.py
@@ -4,6 +4,6 @@ The core classes and functions of Glycosylator
 
 from glycosylator.core.molecule import Molecule
 from glycosylator.core.scaffold import Scaffold
-from glycosylator.core.linkeage import Recipe, Patch
+from glycosylator.core.linkage import Recipe, Patch
 
 __all__ = ["Molecule", "Scaffold", "Recipe", "Patch"]
