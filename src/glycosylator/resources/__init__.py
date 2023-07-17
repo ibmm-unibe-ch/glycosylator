6afd4327b55e65e293cbf437cd29fecdc2f48d73
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-07-17 17:34:14 +0200

    add sequons module again

--- src/glycosylator/resources/__init__.py
+++ src/glycosylator/resources/__init__.py
@@ -5,3 +5,4 @@ Additional glycosylator resources
 from glycosylator.resources.pdbe_compounds import *
 from glycosylator.resources.icons import *
 from glycosylator.resources.names import id_to_name
+from glycosylator.resources.sequons import *
