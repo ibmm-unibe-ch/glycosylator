4319bfb93ce2fd16665a5dcbdc64644e7fa60c3e
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-07-17 17:09:57 +0200

    add name mappings for iupac strings +++ update icon mappings +++ fix icon typo

--- src/glycosylator/resources/__init__.py
+++ src/glycosylator/resources/__init__.py
@@ -4,3 +4,4 @@ Additional glycosylator resources
 
 from glycosylator.resources.pdbe_compounds import *
 from glycosylator.resources.icons import *
+from glycosylator.resources.names import id_to_name
