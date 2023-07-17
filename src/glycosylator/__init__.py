cd7b2fbfdf1d6d1d46a13ac4fa538ff6abe72d13
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-07-17 17:12:42 +0200

    update version

--- src/glycosylator/__init__.py
+++ src/glycosylator/__init__.py
@@ -1,9 +1,10 @@
+from glycosylator.core import *
+import glycosylator.utils.visual as visual
+
 # from biobuild import load_sugars
 from biobuild.core import *
 from biobuild.resources import *
 
-
-from glycosylator.core import *
 
 # this module overwrites some of the imports from the biobuild resources module
 from glycosylator.resources import *
@@ -11,5 +12,5 @@ from glycosylator.resources import *
 # from the resources module
 load_glycosylator_compounds()
 
-__version__ = "4.0.12"
+__version__ = "4.1.0"
 __author__ = "Noah Kleinschmidt"
