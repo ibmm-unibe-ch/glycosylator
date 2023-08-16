be0625529cb82163657594a0c47b6b0389aee68d
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-08-16 17:12:47 +0200

    update imports

--- src/glycosylator/core/__init__.py
+++ src/glycosylator/core/__init__.py
@@ -3,4 +3,4 @@ The core classes and functions of Glycosylator
 """
 
 from glycosylator.core.Glycan import *
-from glycosylator.core.Scaffold import Scaffold
+from glycosylator.core.Scaffold import *
