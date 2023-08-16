291e14e5b1ffde960e78cdea5c9af4fe96b8a8cc
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-08-16 17:13:37 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="glycosylator",
-    version="4.1.18",
+    version="4.3.22",
     author="Noah H. Kleinschmidt",
     author_email="noah.kleinschmidt@students.unibe.ch",
     description="A python package for structural simulation of protein glycosylation",
