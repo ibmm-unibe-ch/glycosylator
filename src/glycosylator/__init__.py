ec3864ba137d112c78ba683a5bfbc4fd195181ea
Noah Kleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-08-26 13:04:57 +0200

    augment topology with new glycosidic linkages +++ refactor imports

--- src/glycosylator/__init__.py
+++ src/glycosylator/__init__.py
@@ -1,16 +1,20 @@
-from biobuild import graphs
-from glycosylator.core import *
+import glycosylator.utils as utils
 import glycosylator.utils.visual as visual
+from glycosylator.core import *
 
 # from biobuild import load_sugars
 from biobuild.core import *
 import biobuild.graphs as graphs
+import biobuild.structural as structural
 
 # this module overwrites some of the imports from the biobuild resources module
 from glycosylator.resources import *
 
+import glycosylator.resources as resources
+import glycosylator.optimizers as optimizers
+
 # from the resources module
 load_glycosylator_compounds()
 
-__version__ = "4.3.22"
+__version__ = "4.3.64"
 __author__ = "Noah Kleinschmidt"
