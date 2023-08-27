40a8550d34463b71eeff16a86430e27bc21cf8b1
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2023-08-27 23:46:56 +0200

    update python reqs

--- setup.py
+++ setup.py
@@ -56,5 +56,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
 )
