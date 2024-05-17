bfc916812d422d26b727ad7b357388ce60653092
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-05-17 15:26:08 +0200

    update init

--- src/glycosylator/__init__.py
+++ src/glycosylator/__init__.py
@@ -2,12 +2,12 @@ import glycosylator.utils as utils
 import glycosylator.utils.visual as visual
 from glycosylator.core import *
 
-# from biobuild import load_sugars
-from biobuild.core import *
-import biobuild.graphs as graphs
-import biobuild.structural as structural
+# from buildamol import load_sugars
+from buildamol.core import *
+import buildamol.graphs as graphs
+import buildamol.structural as structural
 
-# this module overwrites some of the imports from the biobuild resources module
+# this module overwrites some of the imports from the buildamol resources module
 from glycosylator.resources import *
 
 import glycosylator.resources as resources
@@ -16,5 +16,5 @@ import glycosylator.optimizers as optimizers
 # from the resources module
 load_glycosylator_compounds()
 
-__version__ = "4.4.0"
+__version__ = "4.5.6"
 __author__ = "Noah Kleinschmidt"
