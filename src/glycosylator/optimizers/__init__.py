02e06efb7ac62efd29050a04a35111c966a6bd75
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-05-22 09:10:29 +0200

    new scaffold graph +++ new scaffold optimizer

--- src/glycosylator/optimizers/__init__.py
+++ src/glycosylator/optimizers/__init__.py
@@ -1,2 +1,3 @@
 from buildamol.optimizers import *
 from glycosylator.optimizers.utils import *
+from glycosylator.optimizers.ScaffoldRotatron import ScaffoldRotatron
--- src/glycosylator/optimizers/utils.py
+++ src/glycosylator/optimizers/utils.py
@@ -2,14 +2,17 @@
 This module contains additional utility functions for the optimizers that are Glycosylator specific.
 """
 
+import buildamol.graphs.ResidueGraph as ResidueGraph
+import buildamol.structural as structural
+import numpy as np
+
 
 def make_scaffold_graph(
     scaffold,
     only_clashing_glycans: bool = True,
-    only_clashes_between_glycans: bool = False,
     slice: int = 0,
     include_root: bool = False,
-) -> tuple["ResidueGraph", list]:
+) -> tuple["ResidueGraph.ResidueGraph", list]:
     """
     Make a ResidueGraph from a glycosylated scaffold in order to optimize the conformations of the attached glycans.
 
@@ -19,8 +22,6 @@ def make_scaffold_graph(
         The glycosylated scaffold to optimize
     only_clashing_glycans: bool
         If True, only glycans that clash with the protein or each other will will contribute rotatable edges.
-    only_clashes_between_glycans: bool
-        If True, only clashes between glycans will be considered. This is useful for optimizing the conformations of glycans that are already in a good position.
     slice: int
         The number of residue connections to slice from each glycan. This will sub-sample the rotatable edges of the glycans. At 0 all rotatable edges are included.
     include_root: bool
@@ -33,73 +34,126 @@ def make_scaffold_graph(
     list
         The rotatable edges of the graph
     """
-    G = scaffold.make_residue_graph(True)
-    _rotatable_edges = []
+    G = scaffold.make_residue_graph()
 
     if only_clashing_glycans:
-        glycan_residues = {
-            glycan: [res for res in glycan.get_residues()]
-            for glycan in scaffold.glycans.values()
-        }
-        _residues_flat = set(
-            [res for glycan in glycan_residues.values() for res in glycan]
+        glycan_gen = (
+            (root, glycan)
+            for (root, glycan) in scaffold.glycans.items()
+            if glycan.count_clashes() > 0
         )
-        _added_glycans = set()
-        clashes = scaffold.find_clashes()
-        if only_clashes_between_glycans:
-            _check = (
-                lambda a, b: a.parent in _residues_flat and b.parent in _residues_flat
-            )
-        else:
-            _check = (
-                lambda a, b: a.parent in _residues_flat or b.parent in _residues_flat
-            )
-
-            for clash in clashes:
-                if _check(*clash):
-                    for i in clash:
-                        glycan = next(
-                            (
-                                glycan
-                                for glycan, residues in glycan_residues.items()
-                                if i.parent in residues
-                            ),
-                            None,
-                        )
-
-                        if glycan is None or glycan in _added_glycans:
-                            continue
-
-                        G.add_edge(glycan.root_residue, glycan.root_atom)
-                        if include_root:
-                            scaffold_root = next(
-                                atom
-                                for atom, _glycan in scaffold.glycans.items()
-                                if glycan == _glycan
-                            )
-                            G.add_edge(scaffold_root, glycan.root_atom)
-                            G.add_edge(scaffold_root.parent, scaffold_root)
-                            _rotatable_edges.append((scaffold_root, glycan.root_atom))
-
-                        if slice > 0:
-                            _rotatable_edges.extend(
-                                _slice_residue_connections(glycan, slice)
-                            )
-                        else:
-                            _rotatable_edges.extend(
-                                glycan.get_residue_connections(direct_by="root")
-                            )
-
-                        _added_glycans.add(glycan)
-
     else:
-        for glycan in scaffold.glycans.values():
-            if slice > 0:
-                _rotatable_edges.extend(_slice_residue_connections(glycan, slice))
-            else:
-                _rotatable_edges.extend(
-                    glycan.get_residue_connections(direct_by="root")
-                )
+        glycan_gen = scaffold.glycans.items()
+
+    _atoms_to_fill_in = []
+    _rotatable_edges = []
+
+    _flat_residues = list(scaffold.get_residues())
+    _res_coords = [res.coord for res in _flat_residues]
+    dists = structural.cdist(_res_coords, _res_coords) < 8.0
+
+    for root, glycan in glycan_gen:
+
+        if len(glycan.residues) > 1:
+            g = glycan.get_residue_graph()
+            g.make_detailed()
+        else:
+            continue
+
+        _rotatable_edges.extend(glycan.get_residue_connections(direct_by="root"))
+
+        root_residue = root.parent
+        neighboring_residues = (
+            _flat_residues[idx]
+            for idx in np.where(dists[_flat_residues.index(root_residue)])[0]
+        )
+        neighboring_atoms = (
+            atom for res in neighboring_residues for atom in res.child_list
+        )
+
+        _atoms_to_fill_in.extend(g.nodes)
+        _atoms_to_fill_in.extend(neighboring_atoms)
+
+        G.remove_nodes_from(g.residues)
+        G.add_nodes_from(_atoms_to_fill_in)
+        G.add_edges_from(g.edges)
+        if include_root:
+            _rotatable_edges.append((root, glycan.root_atom))
+            G.add_edge(root, glycan.root_atom)
+
+        _atoms_to_fill_in.clear()
+
+    if slice > 0:
+        _rotatable_edges = G.sample_edges(
+            _rotatable_edges, len(scaffold.glycans), slice
+        )
+
+    # if only_clashing_glycans:
+    #     glycan_residues = {
+    #         glycan: [res for res in glycan.get_residues()]
+    #         for glycan in scaffold.glycans.values()
+    #     }
+    #     _residues_flat = set(
+    #         [res for glycan in glycan_residues.values() for res in glycan]
+    #     )
+    #     _added_glycans = set()
+    #     clashes = scaffold.find_clashes()
+    #     if only_clashes_between_glycans:
+    #         _check = (
+    #             lambda a, b: a.parent in _residues_flat and b.parent in _residues_flat
+    #         )
+    #     else:
+    #         _check = (
+    #             lambda a, b: a.parent in _residues_flat or b.parent in _residues_flat
+    #         )
+
+    #         for clash in clashes:
+    #             if _check(*clash):
+    #                 for i in clash:
+    #                     glycan = next(
+    #                         (
+    #                             glycan
+    #                             for glycan, residues in glycan_residues.items()
+    #                             if i.parent in residues
+    #                         ),
+    #                         None,
+    #                     )
+
+    #                     if glycan is None or glycan in _added_glycans:
+    #                         continue
+
+    #                     G.add_edge(glycan.root_residue, glycan.root_atom)
+    #                     if include_root:
+    #                         scaffold_root = next(
+    #                             atom
+    #                             for atom, _glycan in scaffold.glycans.items()
+    #                             if glycan == _glycan
+    #                         )
+    #                         G.add_edge(scaffold_root, glycan.root_atom)
+    #                         G.add_edge(scaffold_root.parent, scaffold_root)
+    #                         _rotatable_edges.append((scaffold_root, glycan.root_atom))
+
+    #                     if slice > 0:
+    #                         _rotatable_edges.extend(
+    #                             _slice_residue_connections(glycan, slice)
+    #                         )
+    #                     else:
+    #                         _rotatable_edges.extend(
+    #                             glycan.get_residue_connections(direct_by="root")
+    #                         )
+
+    #                     _added_glycans.add(glycan)
+
+    # else:
+    #     for glycan in scaffold.glycans.values():
+    #         if slice > 0:
+    #             _rotatable_edges.extend(_slice_residue_connections(glycan, slice))
+    #         else:
+    #             _rotatable_edges.extend(
+    #                 glycan.get_residue_connections(direct_by="root")
+    #             )
+
+    # G.add_atomic_bonds(_rotatable_edges)
 
     return G, _rotatable_edges
 
@@ -129,14 +183,17 @@ def _slice_residue_connections(glycan, slice: int):
 if __name__ == "__main__":
     import glycosylator as gl
 
-    scaf = gl.Scaffold.load(
-        "/Users/noahhk/GIT/glycosylator/src/glycosylator/__figure_makery/scaf_glycosylated.optimized.5.pkl"
+    rot = gl.utils.load_pickle(
+        "/Users/noahhk/GIT/glycosylator/test.scaf.optimize.distrot-raworig.pkl"
     )
+    scaf = rot.graph._molecule
+    # scaf = gl.Scaffold.load(
+    #     "/Users/noahhk/GIT/glycosylator/src/glycosylator/__figure_makery/scaf_glycosylated.optimized.5.pkl"
+    # )
 
     graph, rotatable_edges = make_scaffold_graph(
         scaf,
-        only_clashing_glycans=True,
-        only_clashes_between_glycans=True,
+        only_clashing_glycans=False,
         slice=5,
         include_root=True,
     )
