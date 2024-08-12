57275348566544cba6ea228878b2e1153c0f7891
NoahHenrikKleinschmidt <noah.kleinschmidt@students.unibe.ch>
2024-08-12 10:01:16 +0200

    [automatic] Version update to 5.6.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "glycosylator"
 copyright = "2023, Noah Kleinschmidt"
 author = "Noah Kleinschmidt, Boris Gusev, and Thomas Lemmin"
-release = "5.6.3"
+release = "5.6.4"
 
 
 import plotly.io as pio
